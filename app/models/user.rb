7bf0d6db35ab22bd33199cfb719bb6bc588f86b7
ryan gum <ryangum1@gmail.com>
2016-02-07 14:45:34 +0100

    added devise login and sign up paths

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-	validates :password, :presence => true,
-	                     :on => :create,
-	                     :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }       
+	# validates :password, :presence => true,
+	#                      :on => :create,
+	#                      :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }       
 end
